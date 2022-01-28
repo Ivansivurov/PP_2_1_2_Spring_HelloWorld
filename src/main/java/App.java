9f806bb827a7ac340896dd7cf31917514504b311
Ivansivurov <buddamaster@mail.ru>
2022-01-28 12:50:22 +0300

    ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = applicationContext.getBean(HelloWorld.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+        Cat catBean3 = applicationContext.getBean(Cat.class);
+        System.out.println(bean == bean1);
+        System.out.println(catBean3 == catBean2);
     }
 }
